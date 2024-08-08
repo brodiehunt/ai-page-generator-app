2841090a09e6219235e3788d75d283d63d75a4e9
Brodie Hunt <62501585+brodiehunt@users.noreply.github.com>
2024-08-09 05:31:20 +1000

    casing problem in import statement cuaseing build to fail...

--- src/app/dashboard/longtail-generator/page.jsx
+++ src/app/dashboard/longtail-generator/page.jsx
@@ -1,4 +1,4 @@
-import LongtailGeneratorClientWrapper from "@/src/components/longtailCreatorPage/LongtailGeneratorClientWrapper";
+import LongtailGeneratorClientWrapper from "@/src/components/longtailCreatorPage/longtailGeneratorClientWrapper";
 import DashboardPageHeader from "@/src/components/shared/dashboard/DashboardPageHeader";
 import DashboardPagesContainer from "@/src/components/shared/dashboard/DashboardPagesContainer";
 
